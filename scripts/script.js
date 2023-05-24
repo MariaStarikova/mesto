5d4e8ac3f630df916db0fe0f13a739c6e520a246
Starikova Maria <maria050200@gmail.com>
2023-05-24 22:28:46 +0500

    исправление ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let openPopupBtn = document.querySelector('.edit-button');
+let openPopupBtn = document.querySelector('.profile__edit-button');
 let editPopup = document.querySelector('.popup');
 let closePopupBtn = document.querySelector('.popup__close-button');
 let profileTitle = document.querySelector('.profile__title');
@@ -6,21 +6,14 @@ let titleInput = document.querySelector('#title-input');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let subtitleInput = document.querySelector('#subtitle-input');
 let editForm = document.querySelector('.popup__form');
-let saveSubmit = document.querySelector('.popup__button');
 
 openPopupBtn.addEventListener('click', function () {
+  titleInput.value = profileTitle.textContent;
+  subtitleInput.value = profileSubtitle.textContent;
   editPopup.classList.add('popup_opened');
 });
 
 closePopupBtn.addEventListener('click', function () {
-  editPopup.classList.remove('popup_opened');
-});
-
-titleInput.value = profileTitle.textContent;
-
-subtitleInput.value = profileSubtitle.textContent;
-
-saveSubmit.addEventListener('click', function () {
   editPopup.classList.remove('popup_opened');
 });
 
@@ -30,4 +23,6 @@ editForm.addEventListener('submit', function (event) {
   profileTitle.textContent = titleInput.value;
 
   profileSubtitle.textContent = subtitleInput.value;
+
+  editPopup.classList.remove('popup_opened');
 });
