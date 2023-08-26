80ee3a5df2308210800cc0c1029bc428bfe4fb18
Starikova Maria <maria050200@gmail.com>
2023-08-26 21:07:15 +0500

    Updates

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const settings = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_underline',
-  errorClass: 'popup__input-error_active'
-};
